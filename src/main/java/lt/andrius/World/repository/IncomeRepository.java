fc4a1cc401cf5f63862971e5998d505723b6b456
Dailius_VCS <dailiusvcs@gmail.com>
2023-01-25 12:07:22 +0200

    Comment overloads in IncomeRepository

--- src/main/java/lt/andrius/World/repository/IncomeRepository.java
+++ src/main/java/lt/andrius/World/repository/IncomeRepository.java
@@ -8,9 +8,9 @@ import java.util.Optional;
 public interface IncomeRepository extends CrudRepository<Income, Integer> {
 
     Optional<Income> findByAmount(double amount);
-    Optional<Income> findByAmount(double amountMin, double amountMax);
+//    Optional<Income> findByAmount(double amountMin, double amountMax);
 
     Iterable<Income> findByDate(String date);
-    Iterable<Income> findByDate(String dateStart, String dateEnd);
+//    Iterable<Income> findByDate(String dateStart, String dateEnd);
 
 }
